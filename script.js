b16eb492c45b8bd67909bdccbae2c25ab02c127e
SpyrosPolemis <spyridon.polemis@my.jcu.edu.au>
2026-02-22 15:06:55 +1000

    Refactor to work dynamically with number of squares

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const drawingBoard = document.querySelector("#drawing-board");
-for(let i = 0; i < 100; i++) {
+const numberOfSquares = 144;
+for(let i = 0; i < numberOfSquares; i++) {
     const square = document.createElement("div")
     square.classList.add("square")
+    square.style.flexBasis=`${100 / Math.sqrt(numberOfSquares)}%`
     drawingBoard.appendChild(square)
 }
