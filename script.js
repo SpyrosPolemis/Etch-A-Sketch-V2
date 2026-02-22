db0d364e83ba623bcd88276b6d772a47332fbb3c
SpyrosPolemis <spyridon.polemis@my.jcu.edu.au>
2026-02-22 15:23:52 +1000

    feat: squares go black on mouseover

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 const drawingBoard = document.querySelector("#drawing-board");
-const numberOfSquares = 144;
+const numberOfSquares = 400;
 for(let i = 0; i < numberOfSquares; i++) {
     const square = document.createElement("div")
     square.classList.add("square")
     square.style.flexBasis=`${100 / Math.sqrt(numberOfSquares)}%`
     drawingBoard.appendChild(square)
 }
+
+const drawingSquares = document.querySelectorAll(".square");
+drawingSquares.forEach((square) => {
+    square.addEventListener("mouseover", () => {
+        square.style["background-color"] = "black"
+    })
+})
