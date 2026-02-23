c93d1eb1fca09fb79277481126c042515284d306
SpyrosPolemis <spyridon.polemis@my.jcu.edu.au>
2026-02-24 00:31:55 +1000

    Bug: Hovering over a colored square while another color is selected will no longer darken the square in the wrong color

--- script.js
+++ script.js
@@ -12,9 +12,11 @@ for(let i = 0; i < numberOfSquares; i++) {
 const drawingSquares = document.querySelectorAll(".square");
 drawingSquares.forEach((square) => {
     square.addEventListener("mouseover", () => {
-        let opacity = Number(square.style["opacity"])
-        opacity += 0.2
-        square.style["opacity"] = opacity
+        if(square.style.backgroundColor == currectColor) {
+            let opacity = Number(square.style["opacity"])
+            opacity += 0.2
+            square.style["opacity"] = opacity
+        }
     })
 })
 
